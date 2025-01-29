fb9d08f533bf9d0f801f47c010dd1a259e0a3e78
naufalzhafran <latifnaufal7@gmail.com>
2025-01-29 15:58:17 +0700

    refactor: remove deprecated activity types and update enums

--- types/constants/profile.ts
+++ types/constants/profile.ts
@@ -1,14 +1,17 @@
 export enum USER_LEVEL_ENUM {
   JAMAAH,
-  AKTIVIS,
-  AKTIVIS_KK,
-  KADER,
-  KADER_INVENTRA,
-  KADER_KOMPROF,
-  KADER_LANJUT,
+  AKTIVIS = 3,
+  KADER = 6,
+  KADER_LANJUT = 10,
 }
 
 export enum GENDER {
   Female = "F",
   Male = "M",
+}
+
+export enum ADMIN_ROLE_ENUM {
+  SUPER_ADMIN,
+  ADMIN,
+  KONSELOR = 20,
 }
