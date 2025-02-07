63eceba21770ff3641355ce60cc0d2372f64be81
naufalzhafran <latifnaufal7@gmail.com>
2025-02-07 16:09:10 +0700

    feat: generate leaderboard model and migrations

---
+++ database/migrations/1738918114421_create_leaderboards_table.ts
@@ -0,0 +1,52 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName1 = 'achievements'
+  protected tableName2 = 'monthly_leaderboards'
+  protected tableName3 = 'lifetime_leaderboards'
+
+  async up() {
+    this.schema.createTable(this.tableName1, (table) => {
+      table.increments('id')
+      table.integer('user_id').references('public_users.id').onDelete('CASCADE')
+
+      table.string('name')
+      table.string('description')
+      table.integer('type')
+      table.integer('score')
+      table.string('proof')
+      table.integer('status')
+
+      table.integer('approver_id').references('admin_users.id').onDelete('CASCADE')
+      table.date('approved_at')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+
+    this.schema.createTable(this.tableName2, (table) => {
+      table.increments('id')
+      table.integer('user_id').references('public_users.id').onDelete('CASCADE')
+      table.date('month')
+      table.integer('score')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+
+    this.schema.createTable(this.tableName3, (table) => {
+      table.increments('id')
+      table.integer('user_id').references('public_users.id').onDelete('CASCADE')
+      table.integer('score')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName1)
+    this.schema.dropTable(this.tableName2)
+    this.schema.dropTable(this.tableName3)
+  }
+}
