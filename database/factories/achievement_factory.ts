814607954cf3c280891de1e3bf08dec0b7a8bfcd
naufalzhafran <latifnaufal7@gmail.com>
2025-02-10 16:35:28 +0700

    chore: add achievement factory

---
+++ database/factories/achievement_factory.ts
@@ -0,0 +1,25 @@
+import factory from '@adonisjs/lucid/factories'
+import Achievement from '#models/achievement'
+import PublicUser from '#models/public_user'
+import AdminUser from '#models/admin_user'
+
+export const AchievementFactory = factory
+  .define(Achievement, async ({ faker }) => {
+    const users = await PublicUser.query().select('id')
+    const admins = await AdminUser.query().select('id')
+    return {
+      user_id: users[Math.floor(Math.random() * (users.length - 1))].id,
+      name: faker.person.fullName(),
+      description: faker.lorem.paragraphs(),
+      achievement_date: faker.date.recent(),
+      type: faker.number.int({ min: 0, max: 1 }),
+      score: faker.number.int({ min: 0, max: 100 }),
+      proof: faker.image.url(),
+      status: faker.number.int({ min: 0, max: 4 }),
+      approver_id: admins[Math.floor(Math.random() * (admins.length - 1))].id,
+      approved_at: faker.date.recent(),
+      created_at: faker.date.recent(),
+      updated_at: faker.date.recent(),
+    }
+  })
+  .build()
